842bbb5ed20cde4b9299fba27145c708df29b856
Saugat Dhungana <riggerbenu.123@gmail.com>
2025-05-04 15:47:18 +0545

    ok

--- class/intermediate/library.py
+++ class/intermediate/library.py
@@ -2,53 +2,75 @@ class Book:
     def __init__(self,title,author):
         self.title=title
         self.author=author
-        self.is_borrowed=True
-        print(f"Book created: '{self.title}' by {self.author}")
-        
-    def borrow(self):
-        if self.is_borrowed:
-            print(f"Sorry, {self.title} is already borrowed")
+        self.is_borrowed = False
+    
+    def borrow_book(self):
+        if self.is_borrowed is True:
+            print(f"{self.title} by {self.author} is already borrowed.")
         else:
-            print(f"You have borrowed '{self.title}'.")
-            
-            
+            print(f"You borrowed {self.title} by {self.author}")
+            self.is_borrowed=True
     def return_book(self):
-        if self.is_borrowed:
-            self.is_borrowed = False
-            print(f"Thank you for returning '{self.title}'.")
+        if self.is_borrowed is True:
+            print(f"Thank you for returning {self.title}")
+            self.is_borrowed=False
         else:
-            print(f"'{self.title}' was not borrowed out.")
+            print("You cant return the book that you dont have borrowed.")
     
     def __str__(self):
-        print("Book Details:")
-        print(f"Title: {self.title}")
-        print(f"Author: {self.author}")
-        status = 'Borrowed' if self.is_borrowed else 'available'
-        print(f"Status: {status}")
-        
-        
-    # def display(self):
-        
-        
-
-        
-
-
-
-class library:
-    def __init__(self):
-        self.books = []
-        print("Library created")
+        return f"Title: {self.title}, Author: {self.author}"
     
-    def addBooks(self,book):
+    def display(self):
+        print(self)
+        if self.is_borrowed is True:
+            print("Status: Not Available.")
+        else:
+            print("Status: Available.")
+
+class Library:
+    def __init__(self,books=[]):
+        self.books=books
+        
+    def add_books(self,book):
         if isinstance(book, Book):
             self.books.append(book)
-            print(f"The {book} is added to the library.")
+            print(f"Added '{book.title} by {book.author}")
         else:
-            print("Error! Can only add Book object to the library")
+            print("Error")
     
-    def list_available_books(self):
-        print("---Available Books---")
-        available_count = 0
+    def available_books(self):
+        available = False
+        print("########## Available Books to borrow. ##########")
         for book in self.books:
             if not book.is_borrowed:
+                book.display()
+                available = True
+        if not available:
+            print("No available books.")
+        print("########## ##########")
+
+    def all_books(self):
+        print("------------All books: ------------")
+        for book in self.books:
+            book.display()
+        print("-----------------------------------")
+        
+            
+        
+
+
+
+book1 = Book("Harry Potter","JK Rowling")
+book2 = Book("Cheena harayeko mancche","Hari Bangsha Acharya")
+# book1.borrow_book()
+# book1.return_book()
+# book1.borrow_book()
+book2.borrow_book()
+
+
+myLibrary = Library([])
+myLibrary.add_books(book1)
+myLibrary.add_books(book2)
+myLibrary.available_books()
+
+
